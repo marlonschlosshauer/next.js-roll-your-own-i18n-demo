681d3098fa9145e56b85f95b4421326cab1317c6
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-10-29 22:02:44 +0100

    Add string interpolation

--- lib/i18n/shared.ts
+++ lib/i18n/shared.ts
@@ -4,5 +4,18 @@ export const isLocale = (locale: string): locale is Locale =>
   ["de", "en"].includes(locale);
 
 export const translate = (dictionary: Dictionary) => {
-  return (key: keyof Dictionary) => dictionary[key] ?? key;
+  return (
+    key: keyof Dictionary,
+    instances?: Record<string, string | number>,
+  ) => {
+    const target = dictionary[key] ?? key;
+
+    if (!instances) {
+      return target;
+    }
+
+    return Object.entries(instances).map(([key, value]) =>
+      target.replace(`{{${key}}}`, `${value}`),
+    );
+  };
 };
